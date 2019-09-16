3f221345180e09edfb763e38799af62c5e66dab3
yinzhema <yma9826@gmail.com>
2019-09-15 23:23:51 -0400

    session 4.2 complete

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func animateImage(){
+        let bounds=self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60.0
+        self.imageToPunch.bounds=CGRect(x: self.imageToPunch.bounds.origin.x+shrinkValue, y: self.imageToPunch.bounds.origin.y+shrinkValue, width: self.imageToPunch.bounds.size.width-shrinkValue, height: self.imageToPunch.bounds.size.height-shrinkValue)
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0, options: [], animations: { self.imageToPunch.bounds=bounds }, completion: nil)
+    }
     
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
@@ -26,7 +32,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        print("I am tapped!!!")
+        animateImage()
     }
     
 }
